e8b1df10f58fb8bf1e4651fe931880859d201be1
Fuma Nama <god63820869@gmail.com>
2026-01-07 20:51:53 +0800

    feat: Improve error message format to support editor jumping

--- docs/lib/source.ts
+++ docs/lib/source.ts
@@ -1,8 +1,7 @@
 import { loader } from "fumadocs-core/source";
-import { createMDXSource } from "fumadocs-mdx";
-import { docs, meta } from "@/.source";
+import { docs } from "collections/server";
 
 export const source = loader({
   baseUrl: "/docs",
-  source: createMDXSource(docs, meta),
+  source: docs.toFumadocsSource(),
 });
